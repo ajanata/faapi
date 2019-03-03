1867631657fc60cb493807c1462409355e4c1c26
Andy Janata <ajanata@gmail.com>
2019-03-03 11:20:05 -0800

    update copyright

--- client.go
+++ client.go
@@ -1,6 +1,6 @@
 /*
  *
- * Copyright (c) 2018, Andy Janata
+ * Copyright (c) 2018-2019, Andy Janata
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted
